d8ac62f8b9bf7a675acc9c7caeedc701f6bde234
Maxim Barannikov <mbarannikov@kamaz.digital>
2024-05-02 04:27:17 +0300

    Added new structures

--- cmd/events_consumer/main.go
+++ cmd/events_consumer/main.go
@@ -3,17 +3,9 @@ package main
 import (
 	"events_consumer/internal/config"
 	"events_consumer/internal/kafka"
-	"fmt"
-	"reflect"
 )
 
 func main() {
-	// Загрузка конфигурации
 	cfg := config.ConfigLoad()
-	fmt.Printf("cfg %#v\n", cfg)
-	fmt.Println("Тип переменной cfg:", reflect.TypeOf(cfg))
-
-	// Пример использования переменной из конфигурации
-	fmt.Printf("Address: %s\n", cfg.KAFKA_BOOTSTRAP_SERVER)
-	kafka.ConsumerMain(cfg) // Вызов функции ConsumerMain() из пакета kafka
+	kafka.ConsumerMain(cfg)
 }
--- internal/models/event.go
+++ internal/models/event.go
@@ -29,21 +29,62 @@ type KafkaMessage struct {
 	BatteryLevel                       float64 `json:"batteryLevel"`
 }
 
+type VehicleStateUpdateRequest struct {
+	CoreVehicleId                      string  `json:"vehicle_id"`
+	Longitude                          float64 `json:"longitude"`
+	Latitude                           float64 `json:"latitude"`
+	Altitude                           int     `json:"altitude"`
+	Satellites                         int     `json:"satellites"`
+	HighResolutionTotalVehicleDistance int     `json:"vehicle_distance"`
+	Ts                                 int64   `json:"external_ts"`
+	Speed                              int     `json:"speed"`
+	FuelLevel                          int     `json:"fuel_level_percent"`
+	BatteryLevel                       float64 `json:"car_battery_level"`
+}
+
 type TelemetryEventRequest struct {
-	Id                                 string
-	Timestamp                          int64
-	KmclBlockIccid                     string
-	KmclVehicleId                      string
-	KmclTraceId                        string
-	EventHeaderHash                    string
-	Longitude                          float64
-	Latitude                           float64
-	Altitude                           int
-	Satellites                         int
-	HighResolutionTotalVehicleDistance int
-	CurrentMileage                     int
-	Ts                                 int64
-	Speed                              int
-	FuelLevel                          int
-	BatteryLevel                       float64
+	Id                                 string  `json:"id"`
+	Timestamp                          int64   `json:"timestamp"`
+	KmclBlockIccid                     string  `json:"kmcl_block_iccid"`
+	KmclVehicleId                      string  `json:"kmcl_vehicle_id"`
+	KmclTraceId                        string  `json:"kmcl_trace_id"`
+	EventHeaderHash                    string  `json:"event_header_hash"`
+	Longitude                          float64 `json:"longitude"`
+	Latitude                           float64 `json:"latitude"`
+	Altitude                           int     `json:"altitude"`
+	Satellites                         int     `json:"satellites"`
+	HighResolutionTotalVehicleDistance int     `json:"high_resolution_total_vehicle_distance"`
+	CurrentMileage                     int     `json:"current_mileage"`
+	Ts                                 int64   `json:"ts"`
+	Speed                              int     `json:"speed"`
+	FuelLevel                          int     `json:"fuel_level"`
+	BatteryLevel                       float64 `json:"battery_level"`
+}
+
+type CoreResponse struct {
+	Error      string `json:"error"`
+	HTTPStatus int64  `json:"http_status"`
+	Result     struct {
+		Items []struct {
+			BlockID        string        `json:"block_id"`
+			Brand          string        `json:"brand"`
+			CreatedDt      string        `json:"created_dt"`
+			Creator        interface{}   `json:"creator"`
+			ID             string        `json:"id"`
+			KmclVehicleID  string        `json:"kmcl_vehicle_id"`
+			LicenseNumber  string        `json:"license_number"`
+			Model          string        `json:"model"`
+			OrganizationID string        `json:"organization_id"`
+			Status         interface{}   `json:"status"`
+			UpdatedDt      string        `json:"updated_dt"`
+			VehicleGroups  []interface{} `json:"vehicle_groups"`
+			Vin            string        `json:"vin"`
+			VinEngine      string        `json:"vin_engine"`
+		} `json:"items"`
+		Page  int64 `json:"page"`
+		Pages int64 `json:"pages"`
+		Size  int64 `json:"size"`
+		Total int64 `json:"total"`
+	} `json:"result"`
+	Success bool `json:"success"`
 }
